8f59d93437f392009f048328135a84ab7b64635b
mazi <chichebewebdev@gmail.com>
2023-02-11 10:26:59 -0800

    ran some error check on name, email and password

--- server/controllers/controllerAuth.js
+++ server/controllers/controllerAuth.js
@@ -1,14 +1,29 @@
 import User from "../models/User.js";
 
 export const register = async (req, res, next) => {
+  const { name, email, password } = req.body;
   try {
-    const user = await new User(req.body);
+    if (!name) {
+      return res.status(400).json({ msg: "Name is required" });
+    }
+    if (!password) {
+      return res.status(400).json({ msg: "Password is required" });
+    }
+    if (!email) {
+      return res.status(400).json({ msg: "Email is required" });
+    }
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.status(400).json({ msg: "Email already existed" });
+    }
+    const user = await new User({ name, email, password });
     user.save();
-    res.status(200).json({ user });
+    return res.status(200).json({ user });
   } catch (error) {
-    // res.status(500).json({ msg: "Error occurred" });
+    console.log(error);
+    return res.status(500).json({ msg: "Error occurred" });
     // the next parameter passes the error from the error handlingmiddleware
-    next(error);
+    // next(error);
   }
 };
 
